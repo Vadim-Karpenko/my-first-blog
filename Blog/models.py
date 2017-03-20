df28a264b2c8cbc5199e58fc2aec2a3cc502e034
Vadim-Karpenko <best.worker.ever@protonmail.com>
2017-03-20 23:46:56 +0200

    My first application using Django framework

---
+++ Blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    published_date = models.DateTimeField(default=timezone.now)
+    
+    def publish(self):
+        self.safe()
+        
+    def __str__(self):
+        return self.title
