af3bf9a00f74462560fab7212ca20e58a9e36332
Vadim-Karpenko <best.worker.ever@protonmail.com>
2017-03-23 02:19:57 +0200

    Add pagination

--- Blog/views.py
+++ Blog/views.py
@@ -1,8 +1,24 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    posts = reversed(Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date'))
+    posts_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date') #Filter every post by date
+    
+    # Make page 1, page 2...
+    paginator = Paginator(posts_list, 3) # Show 3 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
+    
+    
     return render(request, 'post_list.html', {'posts': posts})
