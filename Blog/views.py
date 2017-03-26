a5ffad77db091fb284e3f2317d885e95b5fa5016
Vadim-Karpenko <best.worker.ever@protonmail.com>
2017-03-26 23:17:27 +0300

    Each post have own page

--- Blog/views.py
+++ Blog/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -22,3 +22,7 @@ def post_list(request):
 
 
     return render(request, 'post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
