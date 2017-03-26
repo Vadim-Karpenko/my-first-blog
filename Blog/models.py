5fe119774ffe0afefa7eb16fdaba74e92a34cba3
Vadim-Karpenko <best.worker.ever@protonmail.com>
2017-03-26 23:55:37 +0300

    Added one column in db, some changes in post_detail.html

--- Blog/models.py
+++ Blog/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
+    short_text = models.CharField(max_length=600)
     text = models.TextField()
     published_date = models.DateTimeField(default=timezone.now)
     
